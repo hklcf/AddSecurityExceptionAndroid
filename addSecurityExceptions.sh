55bd8e1f9aae2c3b5ed61673b8dd2ac705151571
HKLCF <hklcfs@gmail.com>
2020-11-16 12:25:41 +0800

    Added the ability to run the script from any directory

--- addSecurityExceptions.sh
+++ addSecurityExceptions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ $# -eq 0 ]; then
   echo "No arguments supplied"
-  echo "Usage: ./addSecurityExceptions.sh APK filename"
+  echo "Usage: $0 <APK filename>"
   exit -1
 fi
 if [ ! -z "$2" ]; then
@@ -22,19 +24,19 @@ apktool="apktool_2.4.1.jar"
 newFileName=$filename$new
 tmpDir=/tmp/$filename
 
-java -jar $apktool d -f -s -o $tmpDir $fullfile
+java -jar "$DIR/$apktool" d -f -s -o $tmpDir $fullfile
 
 if [ ! -d "$tmpDir/res/xml" ]; then
   mkdir $tmpDir/res/xml
 fi
 
-cp network_security_config.xml $tmpDir/res/xml/.
+cp "$DIR/network_security_config.xml" $tmpDir/res/xml/.
 if ! grep -q "networkSecurityConfig" $tmpDir/AndroidManifest.xml; then
   sed -E "s/(<application.*)(>)/\1 android\:networkSecurityConfig=\"@xml\/network_security_config\" \2 /" $tmpDir/AndroidManifest.xml > $tmpDir/AndroidManifest.xml.new
   mv $tmpDir/AndroidManifest.xml.new $tmpDir/AndroidManifest.xml
 fi
 
-java -jar $apktool empty-framework-dir --force $tmpDir
+java -jar "$DIR/$apktool" empty-framework-dir --force $tmpDir
 echo "Building new APK $newFileName"
-java -jar $apktool b -o $newFileName $tmpDir
+java -jar "$DIR/$apktool" b -o $newFileName $tmpDir
 jarsigner -verbose -keystore $debugKeystore -storepass android -keypass android $newFileName androiddebugkey
